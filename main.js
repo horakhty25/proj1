0c671ccef0b451c258429329e9b487657676f29b
horakhty25 <113999711+horakhty25@users.noreply.github.com>
2022-09-20 21:50:48 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,42 @@
-// var a = welcome to my website;
-// document.write(a)
+var a = 'welcome to my website'
+var b = 'this is just a single website, so no links will work'
+// confirm  (b)
+
+
+function menubox() {
+    var reveals = document.querySelectorAll(".menubox");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", menubox);
+
+  
+
+  function btn2body() {
+    var reveals = document.querySelectorAll(".btn2body");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 170;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", btn2body);
